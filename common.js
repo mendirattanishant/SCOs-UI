16a0e21146d3847e1af00ca7eb34cefd42003fc6
Nishant Mendiratta <nishant.mendiratta89@gmail.com>
2016-03-02 20:32:11 -0800

    SCOs in HTML5

---
+++ common.js
@@ -0,0 +1,53 @@
+function readCookie(name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
+}
+
+// function getQS(qsParam) {
+// 	var qs = window.location.search;
+// 	var qsValue = 0;
+// 	if(qs) {
+
+// 	}
+
+// 	 qs.split(qsParam)[1] ? window.location.search.split(qsParam)[1] : 0;
+// 	 if (qsValue != 0) {
+// 	 	qsValue.split("&")[0]
+// 	 }
+// 	 alert(qsParam + "::" + qsValue);
+// 	 return parseInt(qsValue);
+// }
+
+function getQS(qsParam)
+{
+    var qsVars = [];
+    var hash;
+
+    if(window.location.search.split(qsParam)[1])
+    {
+	    var hashes = window.location.search.slice(window.location.search.indexOf('?') + 1).split('&');
+	    for(var i = 0; i < hashes.length; i++)
+	    {
+	        hash = hashes[i].split('=');
+	        if(hash[0] == qsParam) {
+	        	return parseInt(hash[1]);
+	        }
+	    }
+	} else {
+		return 0;
+	}
+}
+
+// function setQS(nextPage, key, value) {
+// 	return nextPage + '?' + key + '=' + value;
+// }
+
+function setQS(url, key, value) {   
+	  if (key) {
+	    var isQuestionMarkPresent = url && url.indexOf('?') !== -1;
+	    var separator = isQuestionMarkPresent ? '&' : '?';
+	    url += separator + key + '=' + value;
+	  }
+	  return url;
+}
